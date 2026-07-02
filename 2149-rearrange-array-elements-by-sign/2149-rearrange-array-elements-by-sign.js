1453c3b879c84daff6fc6c4f332ced8bb50a7273
Hari Reshmi <harireshmi22@gmail.com>
2026-07-02 23:57:37 +0530

    Time: 13 ms (80.16%), Space: 99.2 MB (35.52%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var rearrangeArray = function(nums) {
+    // Technique 1: Two Pointers with Extra Array - Most Common
+
+    let n = nums.length; 
+    let posIndex = 0; 
+    let negIndex = 1; 
+    let arr = new Array(n)
+
+    for(let i = 0; i < n; i++) {
+        let num = nums[i]; 
+
+        // if num is greater than negative value, increase by 2
+        // because of index position: posIndex = 0, 2, 4
+        // and negIndex = 1, 3, 5, we will replace the negative value with negIndex
+        if(num > 0) {
+            arr[posIndex] = num; 
+            posIndex += 2; 
+        } else {
+            arr[negIndex] = num; 
+            negIndex += 2; 
+        }
+    }
+
+    return arr
+};
