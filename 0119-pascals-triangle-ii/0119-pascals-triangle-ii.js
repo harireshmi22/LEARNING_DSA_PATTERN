3dfb4b6f5ce231910cb4c67a35ce2a198efb2887
Hari Reshmi <harireshmi22@gmail.com>
2026-07-03 18:17:13 +0530

    Time: 0 ms (100%), Space: 53.6 MB (62.42%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.js
@@ -3,27 +3,37 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-    const triangle = []; 
-    triangle.push([1]); 
 
-    for(let i = 1; i <= rowIndex + 1; i++) {
-        const prevRow = triangle[i - 1]; 
-        const currRow = []; 
-        currRow.push(1); 
+    // indexes se triangle create karke bana rhe hai 
+    let row = [1]
 
-        for(let j = 1; j < i; j++) {
-            const sum = prevRow[j - 1] + prevRow[j]; 
-            currRow.push(sum)
+    for(let i = 1; i <= rowIndex; i++){
+        row.push(1)
+
+        for(j = i - 1; j > 0; j--) {
+            row[j] = row[j - 1] + row[j]
         }
-
-        currRow.push(1); 
-        triangle.push(currRow); 
     }
 
-    return triangle[rowIndex]; 
+    return row; 
 };
 
-// function getRowIndex(rowIndex) {
-//     const fullTriangle = getRow(rowIndex + 1); 
-//     return fullTriangle[rowIndex]; 
-// }
+// var getRow = function(rowIndex) {
+//     const triangle = [];
+//     triangle.push([1]);
+
+//     for(let i = 1; i <= rowIndex; i++) { // <= use karo, clean lagta hai
+//         const prevRow = triangle[i - 1];
+//         const currRow = [];
+//         currRow.push(1);
+
+//         for(let j = 1; j < i; j++) {
+//             currRow.push(prevRow[j - 1] + prevRow[j]);
+//         }
+
+//         currRow.push(1);
+//         triangle.push(currRow);
+//     }
+
+//     return triangle[rowIndex]; // count hata diya
+// };
