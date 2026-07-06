045f25b75c645ef9d8834020bfe17617820be994
Hari Reshmi <harireshmi22@gmail.com>
2026-07-06 18:23:31 +0530

    Time: 17 ms (69.46%), Space: 63.5 MB (46.4%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -7,7 +7,12 @@ var subarraySum = function (nums, k) {
 
     // prefix technique
     let n = nums.length;
+
     let map = new Map();
+
+    /* Map: { 0: 1 } (Humne zero ko frequency 1 ke sath pehle hi daal diya hai. Yeh humesha starting base state hoti hai). */
+    map.set(0, 1);
+
     let count = 0;
     let currentSum = 0;
 
@@ -33,22 +38,30 @@ var subarraySum = function (nums, k) {
     // currentSum = 0
     // Map: { 0: 1 } (Humne zero ko frequency 1 ke sath pehle hi daal diya hai. Yeh humesha starting base state hoti hai).
 
+    /* Step 1: i = 0, Element = 3
+    currentSum update karein: currentSum = currentSum + 3 $\rightarrow$ 0 + 3 = 3
+    Target (jo map me check karna hai): Target = currentSum - k $\rightarrow$ 3 - 7 = -4
+    Map Check: Kya Map ke paas -4 hai? Nahi (map.has(-4) is false). Toh count me kuch add nahi hoga (count = 0).
+    Map Update: Hum currentSum (3) ko map me insert karenge. Kyunki 3 pehli baar aaya hai, iski frequency 1 set hogi.
+    Map State: { 0: 1, 3: 1 } */
 
-        map.set(0, 1);
     // With the help of hashmap 
     for (let i = 0; i < n; i++) {
-        // base case
 
+        // current sum = currentSum ki value add karo nums[i]
         currentSum += nums[i];
 
+        // agar current - k karke jo value niklegi aur map me hai toh count ek badha do 
         if (map.has(currentSum - k)) {
+            // map se 1 nikalkar add kardi
             count += map.get(currentSum - k);
         }
-
+        
+        // map me currentSum ki value and index ke saath add kardo
         map.set(currentSum, (map.get(currentSum) || 0) + 1);
     }
 
-
+    // count daalo 
     return count;
 
 };
