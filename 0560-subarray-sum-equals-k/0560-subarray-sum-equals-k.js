b41de6d955fd363bc45f2decc24e7d84cf5134d4
Hari Reshmi <harireshmi22@gmail.com>
2026-07-06 14:59:46 +0530

    Time: 18 ms (63.14%), Space: 62.3 MB (66.16%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.js
@@ -7,23 +7,47 @@ var subarraySum = function (nums, k) {
 
     // prefix technique
     let n = nums.length;
+    let map = new Map();
     let count = 0;
     let currentSum = 0;
-    let prefixSum = [];
 
+    // With the help of array TC 0(n^2) SC 0(n)
+    // for (let i = 0; i < n; i++) {
+    //     currentSum += nums[i];
+    //     prefixSum[i] = currentSum;
+    // }
+
+    // // count subarrays
+    // for (let i = 0; i < prefixSum.length; i++) {
+    //     // base case 
+    //     if (prefixSum[i] === k) count++;
+
+    //     for(let j = 0; j < i; j++)
+    //     if(prefixSum[i] - prefixSum[j] === k) count++
+    // }
+
+
+
+    //     Initial State (Start hone se pehle)
+    // count = 0
+    // currentSum = 0
+    // Map: { 0: 1 } (Humne zero ko frequency 1 ke sath pehle hi daal diya hai. Yeh humesha starting base state hoti hai).
+
+
+        map.set(0, 1);
+    // With the help of hashmap 
     for (let i = 0; i < n; i++) {
+        // base case
+
         currentSum += nums[i];
-        prefixSum[i] = currentSum;
+
+        if (map.has(currentSum - k)) {
+            count += map.get(currentSum - k);
+        }
+
+        map.set(currentSum, (map.get(currentSum) || 0) + 1);
     }
 
-    // count subarrays
-    for (let i = 0; i < prefixSum.length; i++) {
-        // base case 
-        if (prefixSum[i] === k) count++;
-
-        for(let j = 0; j < i; j++)
-        if(prefixSum[i] - prefixSum[j] === k) count++
-    }
 
     return count;
 
