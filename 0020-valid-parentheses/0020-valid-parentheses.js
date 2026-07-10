680655b9dada64941bd1c9befe662bc1fa66605f
Hari Reshmi <harireshmi22@gmail.com>
2026-07-10 14:25:56 +0530

    Time: 6 ms (21.23%), Space: 56 MB (44.43%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    let stack = []; 
+
+    for(let char of s) {
+        if(char === "(" || char === "{" || char === "[") {
+            stack.push(char);
+        } else {
+            if(stack.length === 0) {
+                return false; 
+            }
+
+            if(char === ")" && stack[stack.length - 1] !== "(") {
+                return false; 
+            }
+
+            if(char === "]" && stack[stack.length - 1] !== "[") {
+                return false; 
+            }
+
+            if(char === "}" && stack[stack.length - 1] !== "{") {
+                return false; 
+            }
+
+            stack.pop(); 
+        } 
+        
+    }  
+
+    return stack.length === 0; 
+};
