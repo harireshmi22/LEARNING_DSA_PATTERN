569af3764a2cb7d8615ee81180a51964e83779e9
Hari Reshmi <harireshmi22@gmail.com>
2026-08-06 09:55:38 +0530

    Time: 0 ms (100%), Space: 54.1 MB (36.9%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let low = 0, high = nums.length - 1; 
+    let ans = nums.length;
+
+    while(low <= high) {
+        let mid = Math.floor(low + (high - low) / 2); 
+
+        if(nums[mid] >= target) {
+            ans = mid; 
+            high = mid - 1; 
+        } else {
+            low = mid + 1; 
+        }
+    }
+
+    return ans; 
+};
