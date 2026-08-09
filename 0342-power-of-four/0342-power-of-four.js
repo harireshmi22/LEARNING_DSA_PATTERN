353df73d6a328e1deb3c4a140e74d25bb6de0931
Hari Reshmi <harireshmi22@gmail.com>
2026-08-09 23:37:58 +0530

    Time: 0 ms (100%), Space: 56.5 MB (37.09%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfFour = function(n) {
+    if(n <= 0) return false; 
+    if(n === 1)  return true; 
+
+    return (n % 4 === 0) && isPowerOfFour(n / 4); 
+};
