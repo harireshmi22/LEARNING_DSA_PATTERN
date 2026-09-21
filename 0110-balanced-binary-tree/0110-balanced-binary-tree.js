f75dfc26bd4de3d738832ae6460564a0d4bb2f18
Hari Reshmi <harireshmi22@gmail.com>
2026-09-21 22:48:42 +0530

    Time: 2 ms (39.68%), Space: 60.9 MB (9.92%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isBalanced = function (root) {
+
+    if (root === null) return true;
+
+    let leftHeight = height(root.left);
+    let rightHeight = height(root.right);
+
+    if (Math.abs(leftHeight - rightHeight) > 1) return false;
+
+    return isBalanced(root.left) && isBalanced(root.right);
+};
+
+
+function height(root) {
+    if (root === null) return 0;
+
+    return 1 + Math.max(height(root.left), height(root.right));
+}
